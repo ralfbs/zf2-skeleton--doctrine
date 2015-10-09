b5e93c6ce2262039aa5fd6097c7f9b203f085108
Ralf Schneider <ralf@hr-interactive.de>
2015-10-09 13:13:13 +0200

    Rest Client and Server

--- module/Application/src/Application/Controller/RestClientController.php
+++ module/Application/src/Application/Controller/RestClientController.php
@@ -49,26 +49,4 @@ class RestClientController extends AbstractActionController
         return new JsonModel($data);
     }
 
-    public function pimCoreCountAction()
-    {
-        //http://www.pimcore.dml/webservice/rest/asset-count?apikey=6e7bb15450a5b974279c631693fedfce76eaefa459f4531e1405ae9e7efb5609
-        $request = new Request();
-        $request->getHeaders()->addHeaders(array(
-            'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8'
-        ));
-        $request->setUri('http://www.pimcore.dml/webservice/rest/asset-count');
-
-        // $request->setMethod('POST');
-        // $request->setPost(new Parameters(array('q' => 'London,uk')));
-
-        $request->setMethod('GET');
-        $request->setQuery(new Parameters(array('apikey'     => '6e7bb15450a5b974279c631693fedfce76eaefa459f4531e1405ae9e7efb5609')));
-
-        // $client = new Client();
-        $client = $this->getServiceLocator()->get('HttpClient');
-        $response = $client->dispatch($request);
-        $data = json_decode($response->getBody(), true);
-
-        return new JsonModel($data);
-    }
 }
